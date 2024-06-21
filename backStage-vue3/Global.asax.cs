bc1b1ec7d72b6ade9b767a84957ca58747181494
Defbuf Fug <defbug1122@gmail.com>
2024-06-21 19:37:43 +0800

    docs: 移除BundleConfig、Scripts檔案、新增LoginController

--- backStage-vue3/Global.asax.cs
+++ backStage-vue3/Global.asax.cs
@@ -17,7 +17,6 @@ namespace backStage_vue3
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
         protected void Application_PostAuthorizeRequest()
         {
